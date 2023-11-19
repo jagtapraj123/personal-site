d07b8aa3c5c5dae25ce611a2446b310b22c0c97e
Raj S. Jagtap <jagtapraj123@gmail.com>
2023-11-19 03:06:53 -0500

    minor lint error fix

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Raj Jagtap");
+    expect(document.title).toBe('Raj Jagtap');
   });
 
   it('can navigate to /about', async () => {
